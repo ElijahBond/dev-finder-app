dcffb578a777de2576cde4107d2c5a256f18456a
ElijahBond <bondarefil@gmail.com>
2023-02-24 16:26:37 +0300

    done header and themeSwitcher

--- src/components/TheHeader/TheHeader.tsx
+++ src/components/TheHeader/TheHeader.tsx
@@ -1,9 +1,12 @@
+import { ThemeSwitcher } from 'components/ThemeSwitcher';
+
 import styles from './TheHeader.module.scss';
 
-interface TheHeaderProps { }
-
-export const TheHeader = ({ }: TheHeaderProps) => (
-  <div className={styles.TheHeader} data-testid="TheHeader">
-    TheHeader Component
+export const TheHeader = () => (
+  <div className={styles.header}>
+    <div className={styles.logo}>
+      Dev Finder
+    </div>
+    <ThemeSwitcher />
   </div>
 );
--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,9 +1,22 @@
+import { ReactComponent as MoonIcon} from 'assets/icon-moon.svg';
+import { ReactComponent as SunIcon } from 'assets/icon-sun.svg';
+import { useEffect, useState } from 'react';
+
 import styles from './ThemeSwitcher.module.scss';
 
-interface ThemeSwitcherProps { }
+export const ThemeSwitcher = () => {
+  const [isDark, setDark] = useState(false);
+  const themeText = isDark ? 'Dark' : "Light";
+  const ThemeIcon = isDark ? SunIcon : MoonIcon;
 
-export const ThemeSwitcher = ({ }: ThemeSwitcherProps) => (
-  <div className={styles.ThemeSwitcher} data-testid="ThemeSwitcher">
-    ThemeSwitcher Component
+  useEffect(() => {
+    document.body.setAttribute('data-theme', isDark ? 'dark' : 'light')
+  }, [isDark])
+
+  return (
+  <div className={styles.switcher} onClick={() => setDark(!isDark)}>
+    <span>{themeText}</span>
+    <ThemeIcon className={styles.icon} />
   </div>
-);
+  )
+};
